ad865b2fa3a561f0adcaad478e56b5b38db403b8
Adam Sellers (sellers3) <sellers3@coventry.ac.uk>
2017-11-06 10:15:47 +0000

    Create shoppinglist.py

---
+++ shoppinglist.py
@@ -0,0 +1,11 @@
+###If shopping list is said runs the shoppingList function###
+if "shopping list" in sentence:
+  
+###Asks user for str input for the list and adds it to the shopping list###
+def shopping(word):
+  while word!="stop"
+    shoppingList = []
+    word = input("What do you want adding to the shopping list? ")
+    shoppingList.append(word)
+  print(shoppingList)
+  
