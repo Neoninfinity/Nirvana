eae86916c81dc16dae61d6b59c6050ea05c14df3
Joshua Silvestre (silves10) <silves10@coventry.ac.uk>
2017-11-08 11:10:22 +0000

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,14 +1,14 @@
 ########## Greeting ##########
-S = input("Press enter when you re ready to begin: ")
-sentence = s #Input
-listgreeting = ["Hi","Hello","Hey","Ahoy there matey","Greetings"]
+s = input("Press enter when you re ready to begin: ")
+sentence = s ###Input
+listgreeting = ["Hi ","Hello ","Hey ","Ahoy there matey ","Greetings "]
 from random import randint
-  random = randint(0, 4) #choose random response 
+random = randint(0, 4) ###choose random response 
 greetrandom = listgreeting[random]
-listgreetques["how are you?","how are things?","how’s it going?","how are ye feeling today?","how do you do?"]
+listgreetques = ["how are you?","how are things?","how’s it going?","how are ye feeling today?","how do you do?"]
 greetques = listgreetques[random]
 OUTPUT = greetrandom + greetques
-print (OUTPUT)
+print (OUTPUT) ###just for testing
 ################################# return output to screen
 
 ListFood["food","eat","hungry","resturant",""]
