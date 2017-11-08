f7cbc630ea5c8d0653420f7b52d2abc0cd80890d
Joshua Silvestre (silves10) <silves10@coventry.ac.uk>
2017-11-08 11:29:45 +0000

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,11 +1,19 @@
+### Lists
+listgreeting = ["Hi ","Hello ","Hey ","Ahoy there matey ","Greetings "]
+listgreetques = ["how are you?","how are things?","how’s it going?","how are ye feeling today?","how do you do?"]
+listfood = ["food","eat","hungry","resturant",""]
+
+
+
+
+
+
 ########## Greeting ##########
 s = input("Press enter when you re ready to begin: ")
 sentence = s.lower() ###Input
-listgreeting = ["Hi ","Hello ","Hey ","Ahoy there matey ","Greetings "]
 from random import randint
 random = randint(0, 4) ###choose random response 
 greetrandom = listgreeting[random]
-listgreetques = ["how are you?","how are things?","how’s it going?","how are ye feeling today?","how do you do?"]
 greetques = listgreetques[random]
 OUTPUT = greetrandom + greetques
 print (OUTPUT) ###just for testing
@@ -14,8 +22,7 @@ print (OUTPUT) ###just for testing
 
 
 
-ListFood = ["food","eat","hungry","resturant",""]
-for ListFood in s:
+for listfood in s:
   OUTPUT =  def foodfunction(s)
   break
   
