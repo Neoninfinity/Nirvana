4ce8aa487b2e1082686ee8408218294541509ba9
Rahul Prashar (prasharr) <prasharr@coventry.ac.uk>
2017-11-12 19:19:04 +0000

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,39 +1,32 @@
-import urllib
-import json as m_json
-sentence=0
-### List of responses
-listgreeting = ["Hi ","Hello ","Hey ","Ahoy there matey ","Greetings "]
-listgreetques = ["how are you?","how are things?","how’s it going?","how are ye feeling today?","how do you do?"]
-### Lists for checking input
-listfood = ["food","eat","hungry","resturant","pizza","burger","fastfood"]
-listgeneral = []
-###
-sentence = input("Press enter when you are ready to begin: ")
-while sentence!="finish":
+def main(sentence):
+    import urllib
+    import json as m_json
+    sentence=0
 
+    ### List of responses
+    listgreeting = ["Hi ","Hello ","Hey ","Ahoy there matey ","Greetings "]
+    listgreetques = ["how are you?","how are things?","how’s it going?","how are ye feeling today?","how do you do?"]
 
+    ### Lists for checking input
+    listfood = ["food","eat","hungry","restaurant","pizza","burger","fastfood"]
+    listgeneral = []
 
+    ###
+    ##sentence = input("Press enter when you are ready to begin: ")
+    while sentence!="finish":
+      sentence = str(sentence)  
+      ########## Greeting ##########
+      print(sentence)
+      sentence = sentence.lower() ###Input
+      from random import randint
+      random = randint(0, 4) ###choose random response 
+      greetrandom = listgreeting[randint(0, len(listgreeting)-1)]
+      greetques = listgreetques[randint(0, len(listgreetques)-1)]
+      OUTPUT = greetrandom + greetques
 
-  ########## Greeting ##########
-  sentence = sentence.lower() ###Input
-  from random import randint
-  random = randint(0, 4) ###choose random response 
-  greetrandom = listgreeting[random]
-  greetques = listgreetques[random]
-  OUTPUT = greetrandom + greetques
-  print (OUTPUT) ###just for testing
-  ################################# return output to screen
-
-
-
-  ###Check if they are talking about food
-  for i in listfood:
-    if i in sentence:
-        OUTPUT =  1
-        print("Suprise")
-  sentence = input("Press enter when you are ready to begin: ")
-  
-  
+      ### OUTPUT THE FINAL RESPONSE
+      return (OUTPUT)
+      sentence = input("Press enter when you are ready to begin: ")
   
   
   
