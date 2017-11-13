b86066f71290ac9c02642d4bb4ed68971d5ef755
Adam Sellers (sellers3) <sellers3@coventry.ac.uk>
2017-11-13 10:09:16 +0000

    Update food.py

--- food.py
+++ food.py
@@ -8,14 +8,11 @@ def food(s):
     keyword  = ["food", "restaurant", "fastfood", "takeaway", "italian", "chinese", "indian", "pizza", "fish", "chips", "burger", "steak", "sandwich", "coffee", "mexican", "pub"]
     if ("restaurant" or "takeaway" or "food" or "fastfood") in s:
         foodType = input("What type of food? ")
-        foodType = urllib.urlencode ( { 'q' : foodType } )
-        response = urllib.urlopen ( 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + foodType + " food" ).read()
-        json = m_json.loads ( response )
-        results = json [ 'responseData' ] [ 'results' ]
-        for result in results:
-            title = result['title']
-            url = result['url']   # was URL in the original and that threw a name error exception
-            print ( title + '; ' + url )
+        from google import *
+        num_page = 1
+        search_results = google.search(foodType, num_page)
+        for result in search_results:
+            print(result.description)
     else:
         print(" ")
 food("i want a restaurant")
