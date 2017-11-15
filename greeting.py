f23e923f09e5031cc091e9588dad6987706ffd4c
Joshua Silvestre (silves10) <silves10@coventry.ac.uk>
2017-11-15 12:39:45 +0000

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,22 +1,16 @@
-def mainpersonality(sentence):
+def greeting(sentence):
     import urllib
     import json as m_json
 
     ### List of responses
     listgreeting = ["Hi ","Hello ","Hey ","Greetings "]
-    listgreetques = ["how are you?","how are things?","how’s it going?","how do you do?"
-    
-    
-    
-    ##sentence = input("Press enter when you are ready to begin: ")
-    while sentence!="finish":  
-      ########## Greeting ##########
-      from random import randint
-      greetrandom = listgreeting[randint(0, len(listgreeting)-1)]
-      greetques = listgreetques[randint(0, len(listgreetques)-1)]
-      OUTPUT = greetrandom + greetques  
-      if sentence == ("how are you?"):
-            OUTPUT = "I've been better...\n Cuphead is a hard game"
-      ### OUTPUT THE FINAL RESPONSE
-      return (OUTPUT)
-      sentence = input("Press enter when you are ready to begin: ")
+    listgreetques = ["how are you?","how are things?","how’s it going?","how do you do?"]
+
+    ########## Greeting ##########
+    from random import randint
+    greetrandom = listgreeting[randint(0, len(listgreeting)-1)]
+    greetques = listgreetques[randint(0, len(listgreetques)-1)]
+    OUTPUT = greetrandom + greetques  
+    ### OUTPUT THE FINAL RESPONSE
+    return (OUTPUT)
+
