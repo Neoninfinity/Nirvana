3f1d7562d9a9d4ed2b24d9d0a909297181b96405
Joshua Silvestre (silves10) <silves10@coventry.ac.uk>
2017-11-15 12:03:12 +0000

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -3,18 +3,14 @@ def mainpersonality(sentence):
     import json as m_json
 
     ### List of responses
-    listgreeting = ["Hi ","Hello ","Hey ","Ahoy there matey ","Greetings "]
-    listgreetques = ["how are you?","how are things?","how’s it going?","how are ye feeling today?","how do you do?"]
-
-    ### Lists for checking input
-    listfood = ["food","eat","hungry","restaurant","pizza","burger","fastfood"]
-    listgeneral = []
-
-    ###
+    listgreeting = ["Hi ","Hello ","Hey ","Greetings "]
+    listgreetques = ["how are you?","how are things?","how’s it going?","how do you do?"
+    
+    
+    
     ##sentence = input("Press enter when you are ready to begin: ")
     while sentence!="finish":  
       ########## Greeting ##########
-      sentence = sentence.lower() ###Input
       from random import randint
       greetrandom = listgreeting[randint(0, len(listgreeting)-1)]
       greetques = listgreetques[randint(0, len(listgreetques)-1)]
