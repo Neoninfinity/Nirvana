4d12cdc9db12f37830741d8eef37084a94c33795
Rahul Prashar (prasharr) <prasharr@coventry.ac.uk>
2017-11-21 17:22:01 +0000

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,15 +1,17 @@
 #``````````````````````````````````````````````````````````````````````````````````````````````````````#!/usr/bin/python3 
-
+# -*- coding: utf-8 -*-
+from keywords import key
+keywords = key
 
 ####### Import functions here 
-from main import main
+from main import *
 
 ## End import
 
 import socket, time         # Import socket module
 
 s = socket.socket()         # Create a socket object
-port = 8002             # Reserve a port for your service.
+port = 8007           # Reserve a port for your service.
 s.bind(("localhost", port))          # Bind to the port
 s.listen(5)                 # Now wait for client connection.
 
@@ -23,11 +25,22 @@ try:
         while True:
             rawmsg = c.recv(1024)          # recieve up to 1024 bytes
             
+            msg = rawmsg.decode('utf-8')
+            brokenString = msg.split()
             ## Keyword checking goes here (if statements)
+            for x in brokenString:
+                if x in keywords.keys():
+                    funcLoad = keywords[x]
+                else:
+                    funcLoad = 0
+            
+            print("msg is",msg)
+            if funcLoad == 1:
+                output= hello(msg)
+            else:
+                output = "Error"
 
-                output= main(rawmsg)
-
-                msg = output   # convert and clean the input
+            msg = output   # convert and clean the input
             
             ## End keyword checking
             
