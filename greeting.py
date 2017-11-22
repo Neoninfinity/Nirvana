067c9b7c7738920dbc12c68e04c49b92296b82df
Joshua Silvestre (silves10) <silves10@coventry.ac.uk>
2017-11-22 18:09:42 +0000

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,16 +1,9 @@
-def greeting(sentence):
-    import urllib
-    import json as m_json
-
-    ### List of responses
-    listgreeting = ["Hi ","Hello ","Hey ","Greetings "]
-    listgreetques = ["how are you?","how are things?","how’s it going?","how do you do?"]
-
-    ########## Greeting ##########
+def hello(sentence):
+    #This function returns a greeting and a question,they are both randomly selected from the following lists.
     from random import randint
+    listgreeting = ["Hi ","Hello ","Hey ","Greetings "]  
+    listgreetques = ["how are you?","how are things?","how’s it going?","how do you do?"]
     greetrandom = listgreeting[randint(0, len(listgreeting)-1)]
     greetques = listgreetques[randint(0, len(listgreetques)-1)]
-    OUTPUT = greetrandom + greetques  
-    ### OUTPUT THE FINAL RESPONSE
-    return (OUTPUT)
-
+    OUTPUT = greetrandom + greetques  #Putting the two responses together
+    return (OUTPUT) #returning the response to the main program
