d0c20c04e9041850c9e10fd6e5071a6f07d9ffae
Joshua Silvestre (silves10) <silves10@coventry.ac.uk>
2017-11-23 15:02:30 +0000

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,36 +1,9 @@
-import urllib
-import json as m_json
-##########################################################################################
-### List of responses
-listgreeting = ["Hi ","Hello ","Hey ","Ahoy there matey ","Greetings "]
-listgreetques = ["how are you?","how are things?","how’s it going?","how are ye feeling today?","how do you do?"]
-
-### Lists for checking input
-listfood = ["food","eat","hungry","restaurant","pizza","burger","fastfood"]
-listgeneral = []
-
-################################################################################################################
-sentence = input("Press enter when you are ready to begin: ")
-while sentence!="finish":
-
-  ########## Greeting ##########
-  sentence = sentence.lower() ###Input
-  from random import randint
-  random = randint(0, 4) ###choose random response 
-  greetrandom = listgreeting[random]
-  greetques = listgreetques[random]
-  OUTPUT = greetrandom + greetques
-  print (OUTPUT) ###just for testing
-  ################################# return output to screen
-
-  ##################################################################################################Check if they are talking about food
-  for i in listfood:
-    if i in sentence:
-        OUTPUT =  def food(sentence)
-        print("Suprise")
-  sentence = input("Press enter when you are ready to begin: ")
-################################################################################################################
-
-##########
-
-
+def hello(sentence):
+    #This function returns a greeting and a question, they are both randomly selected from the following lists.
+    from random import randint
+    listgreeting = ["Hi ","Hello ","Hey ","Greetings "]  
+    listgreetques = ["how are you?","how are things?","how’s it going?","how do you do?"]
+    greetrandom = listgreeting[randint(0, len(listgreeting)-1)]
+    greetques = listgreetques[randint(0, len(listgreetques)-1)]
+    OUTPUT = greetrandom + greetques  #Putting the two responses together
+    return (OUTPUT) #returning the response to the main program
